1e46530154acc2b91af27cdf3a8449aea00f4181
Javier González <javigon93@gmail.com>
2020-10-02 11:41:23 +0200

    cosas nuevas

---
+++ DESARROLLO SERVIDOR/T4/ejercicio1.php
@@ -0,0 +1,9 @@
+<?php
+
+// Ejercicio 1
+// Crea una clase Empleado con 2 propiedades: nombre y sueldo. Implementa
+// los getters y setters de las dos propiedades (ten en cuenta la visibilidad
+// adecuada de las propiedades y métodos).
+// Crea dos empleados rellenando su nombre y sueldo, y haz que salga por
+// pantalla la frase “nombre_empleado tiene un sueldo de sueldo_empleado”. 
+
