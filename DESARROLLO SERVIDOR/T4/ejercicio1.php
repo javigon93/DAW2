bd9545e408dda18fe212ebd26d42d07152b3961c
Javier González <javigon93@gmail.com>
2020-10-04 10:32:40 +0200

    añadir tema 7 y workspace

--- DESARROLLO SERVIDOR/T4/ejercicio1.php
+++ DESARROLLO SERVIDOR/T4/ejercicio1.php
@@ -7,3 +7,47 @@
 // Crea dos empleados rellenando su nombre y sueldo, y haz que salga por
 // pantalla la frase “nombre_empleado tiene un sueldo de sueldo_empleado”. 
 
+class Empleado
+{
+
+
+    private $nombre;
+    private $sueldo;
+
+    public function getNombre()
+    {
+
+        return $this->nombre;
+    }
+
+    public function getSueldo()
+    {
+
+        return $this->sueldo;
+    }
+
+
+    public function setNombre($nombre)
+    {
+
+        $this->nombre = $nombre;
+    }
+
+    public function setSueldo($valor){
+
+        $this->sueldo=$valor;
+
+
+
+
+    }
+}
+
+$empleado1= new Empleado();
+$empleado1->setNombre("pepinho");
+$empleado1->setSueldo(250);
+$nombre_empleado=$empleado1->getNombre();
+$sueldo_empleado=$empleado1->getSueldo();
+
+
+echo "Nuestro empleado se llama ".$nombre_empleado." y su sueldo es de ".$sueldo_empleado."€"; 
