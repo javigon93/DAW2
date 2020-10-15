e6653151a3ab5215f45b4e3487b0dbd36240d60a
Javier González <javigon93@gmail.com>
2020-10-15 09:23:47 +0200

    Cosas de Despligue nuevas

--- DESPLIEGUE/sh sensibles/deploy.sh
+++ DESPLIEGUE/sh sensibles/deploy.sh
@@ -3,6 +3,12 @@
 nombre=$(cat package.json | grep "name" | cut -d'"' -f4)
 
 rm -rf c:/xampp/htdocs/CLASE/$nombre
+rm -rf c:/xampp/htdocs/php-server/$nombre
 mkdir c:/xampp/htdocs/CLASE/$nombre
+mkdir c:/xampp/php-server/$nombre
 
 cp -r dist/* c:/xampp/htdocs/CLASE/$nombre
+cp -r src/php/* c:/xampp/php-server/$nombre
+
+
+# revisar si funciona
